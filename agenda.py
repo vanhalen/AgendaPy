36702fe66c8d116ca8d6c7c259d7147d8bd5223c
Rodrigo Chagas <rodrigorrchagas@gmail.com>
2021-12-08 02:52:49 -0300

    feat: CRUD

--- agenda.py
+++ agenda.py
@@ -11,6 +11,13 @@ AGENDA['Rafael'] = {
     'endereco': 'Av. Eloah Cabral Saueia, 251',
 }
 
+# def valida_contato(contato):
+#     if (contato in AGENDA):
+#         return True
+#     else:
+#         print("Esse contato não existe!")
+#         return False
+
 
 def mostrar_contatos():
     for contato in AGENDA:
@@ -28,16 +35,46 @@ def buscar_contato(contato):
         print("Esse contato não existe!")
 
 
-def incluir_contato(contato, telefone, email, endereco):
+def salvar_contato(contato, telefone, email, endereco, tipo):
     AGENDA[contato] = {
         'telefone': telefone,
         'email': email,
         'endereco': endereco,
     }
-    print("Contato {} adicionado com sucesso!".format(contato))
+    if (tipo == 'i'):
+        tipo = 'criado'
+    elif (tipo == 'u'):
+        tipo = 'editado'
+    else:
+        tipo = 'criado/editado'
+
+    print("Contato {} {} com sucesso!".format(contato, tipo))
 
 
-# mostrar_contatos()
-incluir_contato('Maria', '11 94555-9988',
-                'maria@maria.com', 'Rua Datileira, 22')
-buscar_contato('Maria')
+def criar_contato(contato, telefone, email, endereco):
+    salvar_contato(contato, telefone, email, endereco, 'i')
+
+
+def editar_contato(contato, telefone, email, endereco):
+    salvar_contato(contato, telefone, email, endereco, 'u')
+
+
+def deletar_contato(contato):
+    if (contato in AGENDA):
+        AGENDA.pop(contato)
+        print("Contato {} deletado com sucesso!".format(contato))
+    else:
+        print("Esse contato não existe!")
+
+
+# buscar_contato('Rodrigo')
+print('####### AGENDA #######')
+criar_contato('Maria', '11 94555-9988',
+              'maria@maria.com', 'Rua Datileira, 22')
+print('######################')
+editar_contato('Rodrigo', '11 91111-2222',
+               'edit@edit.com', 'Rua Editada, 589')
+print('######################')
+deletar_contato('Rafael')
+print('######################')
+mostrar_contatos()
